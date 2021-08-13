d990d166efc7d0ebe9aa299e04b69530cc4e27c1
xiaojinlu1990@163.com <xiaojinlu1990@163.com>
2021-08-13 16:17:21 +0800

    init面试算法  以及一切

---
+++ src/main/java/com/xjl/everyinterview/EveryinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.xjl.everyinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryinterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryinterviewApplication.class, args);
+    }
+
+}
